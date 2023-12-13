23006dae889c397d95937443bb5597c0e68043ec
Rubenhf <blqckaus@gmail.com>
2023-12-13 22:22:31 +0100

    rep

--- app-classification-pnns1/main.py
+++ app-classification-pnns1/main.py
@@ -19,7 +19,7 @@ def load_model(model_save_path):
         preprocess_input = pickle.load(f)
     return model, preprocess_input
 
-loaded_model, loaded_preprocess_input = load_model("./model_classification_best_weights.h5")
+loaded_model, loaded_preprocess_input = load_model("model/model_classification_best_weights.h5")
 
 def preprocess_image(image, preprocess_input):
     # Apply the same preprocessing as during training
