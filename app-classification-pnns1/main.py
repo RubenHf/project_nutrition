1b002223a1b91311435d97a466514cb9cd0ac914
RubenHf <blqckaus@gmail.com>
2023-12-17 19:03:02 +0100

    pnns1 and 2 models are treated by the same app. Reduction of code. Reduction of package usage. Without pandas and numpy

--- app-classification-pnns1/main.py
+++ app-classification-pnns1/main.py
@@ -4,9 +4,6 @@ from io import BytesIO
 import base64
 import tensorflow as tf
 import pickle
-import pandas as pd
-
-import numpy as np
 
 app = FastAPI()
 
@@ -15,11 +12,16 @@ app = FastAPI()
 
 def load_model(model_save_path):
     model = tf.keras.models.load_model(model_save_path)
-    with open("model/preprocess_input.pkl", "rb") as f:
-        preprocess_input = pickle.load(f)
-    return model, preprocess_input
+    return model
 
-loaded_model, loaded_preprocess_input = load_model("model/model_classification_best_weights.h5")
+def load_preprocess():
+    with open("models/preprocess_input.pkl", "rb") as f:
+        preprocess_input = pickle.load(f)
+    return preprocess_input
+
+loaded_model_pnns1 = load_model("models/model_classification_pnns1_best_weights.h5")
+loaded_model_pnns2 = load_model("models/model_classification_pnns2_best_weights.h5")
+loaded_preprocess_input = load_preprocess()
 
 def preprocess_image(image, preprocess_input):
     # Apply the same preprocessing as during training
@@ -39,11 +41,22 @@ def predict_on_image(model, image_path, preprocess_input):
     predictions = model.predict(preprocessed_image)
     return predictions
 
-def img_prediction_model_pnns(model_pnns, image, preprocess_input):
+def img_prediction_model_pnns(model_pnns, pnns_groups, image, preprocess_input):
     
-    list_pnns = ['Composite foods', 'Fruits and vegetables', 'Cereals and potatoes', 'Fat and sauces',
+    if pnns_groups == "pnns_groups_1":
+        list_pnns = ['Composite foods', 'Fruits and vegetables', 'Cereals and potatoes', 'Fat and sauces',
                     'Salty snacks', 'Beverages', 'Sugary snacks', 'Fish Meat Eggs', 'Milk and dairy products']
-        
+    
+    elif pnns_groups == "pnns_groups_2":
+        list_pnns = ['Pizza pies and quiches', 'Fruits', 'Bread', 'Dressings and sauces', 'Salty and fatty products',
+                     'Sweetened beverages', 'Sweets', 'Biscuits and cakes', 'Pastries', 'Fish and seafood',
+                     'One-dish meals', 'Appetizers', 'Cheese', 'Fats', 'Processed meat', 'Sandwiches',
+                     'Vegetables', 'Breakfast cereals', 'Chocolate products', 'Unsweetened beverages',
+                     'Dried fruits', 'Meat', 'Cereals', 'Eggs', 'Plant-based milk substitutes', 'Legumes',
+                     'Teas and herbal teas and coffees', 'Dairy desserts', 'Nuts', 'Fruit juices',
+                     'Milk and yogurt', 'Artificially sweetened beverages', 'Potatoes', 'Soups', 'Ice cream',
+                     'Offals', 'Waters and flavored waters', 'Fruit nectars']
+
     result = predict_on_image(model_pnns, image, preprocess_input)
     
     image_proba = []
@@ -51,34 +64,34 @@ def img_prediction_model_pnns(model_pnns, image, preprocess_input):
     for pnns, proba in zip(list_pnns, result[0]):
         image_proba.append([pnns, proba])
 
-    image_proba = pd.DataFrame(image_proba, columns=["pnns_groups_2", "probabilities"])
-    
-    image_proba.sort_values("probabilities", ascending = False, inplace = True)
+    # Using a list of dictionaries
+    image_proba = list(zip(list_pnns, result[0].tolist()))
+    image_proba.sort(key=lambda x: x[1], reverse=True)
+
+    # Convert the sorted list of tuples to a list of dictionaries
+    image_proba = [{"pnns_groups": pnns, "probabilities": proba} for pnns, proba in image_proba]
     
     return image_proba
+
+# Alternative to numpy.argmax 
+def argmax(result):
+    return max(range(len(result)), key=result.__getitem__)
 
 def process_image(file_contents: bytes, file_extension: str):
 
     result = predict_on_image(loaded_model, BytesIO(file_contents), loaded_preprocess_input)
 
-    result = np.argmax(result)
-
+    #result = np.argmax(result)
+    result = argmax(result)
+    
     # Return a dictionary or any other data you want
     return {"status": "success", "result": str(result)}
 
-def best_predictions(model_pnns, file_contents: bytes, preprocess_input, threshold = 1):
-    image_proba_df = img_prediction_model_pnns(model_pnns, BytesIO(file_contents), preprocess_input)
-    
-    # We filter on the threshold (we want the pnns with a cummulated proba of "threshold")
-    index = image_proba_df[image_proba_df["probabilities"].cumsum() <= threshold].index
-    
-    image_proba_df = image_proba_df[image_proba_df.index.isin(index)]
-    
-    return image_proba_df
-
 @app.post("/process-image/")
-async def process_image_endpoint(file: UploadFile = File(None), 
-    base64_image: str = Form(None)):
+async def process_image_endpoint(
+    file: UploadFile = File(None), 
+    base64_image: str = Form(None), 
+    pnns_groups: str = Form(None)):
 
     try:
         if file:
@@ -100,13 +113,15 @@ async def process_image_endpoint(file: UploadFile = File(None),
             encoded_data = data_split[1]
             
             file_contents = base64.b64decode(encoded_data)
-            
+
         # We do the prediction
-        df_result = best_predictions(loaded_model, file_contents, loaded_preprocess_input)
-
-        df_result = df_result.to_dict(orient='records')
-
-        return JSONResponse(content=df_result)
+        if pnns_groups == "pnns_groups_1":
+            result = img_prediction_model_pnns(loaded_model_pnns1, pnns_groups, BytesIO(file_contents), loaded_preprocess_input)
+        elif pnns_groups == "pnns_groups_2":
+            result = img_prediction_model_pnns(loaded_model_pnns2, pnns_groups, BytesIO(file_contents), loaded_preprocess_input)
+        
+        #df_result = df_result.to_dict(orient='records')
+        return JSONResponse(content=result)
 
     except Exception as e:
         return JSONResponse(content={"status": "error", "message": str(e)})
