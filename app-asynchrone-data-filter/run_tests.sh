7175609bfbe4bed0df8232ccb0b91a35592cf10e
RubenHf <blqckaus@gmail.com>
2024-01-10 14:09:58 +0100

    Changed in S3 access, added test to local and during deployment

--- app-asynchrone-data-filter/run_tests.sh
+++ app-asynchrone-data-filter/run_tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Script run_tests.sh..."
 set -e  # Exit on error
 
 # Install dependencies
+echo "Installing requirements..."
 pip install -r requirements.txt
 
 # Run tests
+echo "Running tests..."
 pytest test_main.py
