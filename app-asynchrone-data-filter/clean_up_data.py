95a53d27d8af3e0184e704b0c8843305d06418e9
RubenHf <blqckaus@gmail.com>
2024-01-10 15:38:20 +0100

    Added a script to launch the clean up of the data

--- app-asynchrone-data-filter/clean_up_data.py
+++ app-asynchrone-data-filter/clean_up_data.py
@@ -13,14 +13,14 @@ files_treating_step_2 = {'data_file_S3' : 'files/cleaned_data_post_urls.csv',
 
 logger.info("[LAUNCHED URLS TREATMENT OF FILE: cleaned_data.csv]")
 
-response = app.post("/process-data-urls/", files_treating_step_1)
+response = app.post("/process-data-urls/", data=files_treating_step_1)
 
 logger.info("[FINISHED TREATMENT...]")
 logger.info("[FILED GENERATED: cleaned_data_post_urls.csv]\n\n")
 
 logger.info("[LAUNCHED IMAGES TREATMENT OF FILE: cleaned_data_post_urls.csv]")
 
-response = app.post("/process-data-image/", files_treating_step_2)
+response = app.post("/process-data-image/", data=files_treating_step_2)
 
 logger.info("[FINISHED TREATMENT...]")
 logger.info("[FILED GENERATED: cleaned_data_post_images.csv]\n\n")
