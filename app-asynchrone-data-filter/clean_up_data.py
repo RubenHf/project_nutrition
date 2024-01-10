dac3801e117850c60766417068dc0718979c7f0c
RubenHf <blqckaus@gmail.com>
2024-01-10 16:20:07 +0100

    Added a script to launch the clean up of the data

--- app-asynchrone-data-filter/clean_up_data.py
+++ app-asynchrone-data-filter/clean_up_data.py
@@ -1,25 +1,25 @@
-from main import app
+import requests
 import logging
-from starlette.requests import FormData
+import os
 
 # Set up logging configuration
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)  
 
+# Read environment variable from Heroku config vars
+url_api = os.environ.get('url_api')
+
 files_treating_step_1 = {'data_file_S3' : 'files/cleaned_data_test.csv',#'files/cleaned_data.csv', 
                   'data_file_S3_post_clean_up' :'files/cleaned_data_post_urls.csv'}
-
-form_data_1 = FormData(files=files_treating_step_1)
 
 files_treating_step_2 = {'data_file_S3' : 'files/cleaned_data_post_urls.csv', 
                   'data_file_S3_post_clean_up' :'files/cleaned_data_post_images.csv'}
 
-form_data_2 = FormData(files=files_treating_step_2)
 
 logger.info("[LAUNCHED URLS TREATMENT OF FILE]")
 logger.info("[cleaned_data.csv]")
 
-response = app.post("/process-data-urls/", files=form_data_1)
+response = requests.post(url_api + "/process-data-urls/", files_treating_step_1)
 
 logger.info("[FINISHED TREATMENT...]")
 logger.info("[FILED GENERATED: cleaned_data_post_urls.csv]\n\n")
@@ -27,7 +27,7 @@ logger.info("[FILED GENERATED: cleaned_data_post_urls.csv]\n\n")
 logger.info("[LAUNCHED IMAGES TREATMENT OF FILE]")
 logger.info("[cleaned_data_post_urls.csv]")
 
-response = app.post("/process-data-image/", files=form_data_2)
+response = requests.post(url_api + "/process-data-image/", files_treating_step_2)
 
 logger.info("[FINISHED TREATMENT...]")
 logger.info("[FILED GENERATED: cleaned_data_post_images.csv]\n\n")
