548e8e2af326a4cf53ef72add446a388a8a9e824
RubenHf <blqckaus@gmail.com>
2024-01-26 11:10:21 +0100

    folder name changed

---
+++ app-classification-pnns-groups/functions/loading_models.py
@@ -0,0 +1,80 @@
+import pickle
+import tensorflow as tf
+import os
+import boto3
+import gc
+
+# Load the model from the path
+def load_model(path):
+    if os.path.exists(path):
+        return tf.keras.models.load_model(path)
+    else:
+        print(f"Error: File '{path}' not found.")
+        return None 
+
+# Load preprocess from the path
+def load_preprocess(path):
+    if os.path.exists(path):
+        with open(path, "rb") as f:
+            return pickle.load(f)
+    else:
+        print(f"Error: File '{path}' not found.")
+        return None 
+
+# Function to load a file from S3
+def download_file_from_s3(bucket_name, object_key, local_file_path):
+    s3 = boto3.client('s3')
+
+    try:
+        # Download object in local from bucket
+        s3.download_file(bucket_name, object_key, local_file_path)
+    except Exception as e:
+        print(f"Error downloading file: {str(e)}")
+
+# Function to remove the files downloaded
+def remove_local_file(file_path):
+    try:
+        if os.path.exists(file_path):
+            # Remove local object
+            os.remove(file_path)
+        else: 
+            print("No file present")
+    except Exception as e:
+        print(f"Error removing file: {str(e)}")
+
+def load_API_models():
+    # S3 bucket from the project and files
+    bucket_name = 'nutritious.app'
+    model1_file = 'developped_models/model_classification_pnns1_best_weights.h5'
+    model2_file = 'developped_models/model_classification_pnns2_best_weights.h5'
+    preprocess_file = 'developped_models/preprocess_input.pkl'
+
+    # Local file paths to save the downloaded files
+    local_model1_path = 'local_model1.h5'
+    local_model2_path = 'local_model2.h5'
+    local_preprocess_path = 'local_preprocess_input.pkl'
+
+
+    print("Downloading files...")
+
+    # We download the files in the app directory
+    download_file_from_s3(bucket_name, model1_file, local_model1_path)
+    download_file_from_s3(bucket_name, model2_file, local_model2_path)
+    download_file_from_s3(bucket_name, preprocess_file, local_preprocess_path)
+
+    print("Loading files...")
+
+    # We load the models and preprocess
+    loaded_model_pnns1 = load_model(local_model1_path)
+    loaded_model_pnns2 = load_model(local_model2_path)
+    loaded_preprocess_input = load_preprocess(local_preprocess_path)
+
+    print("Deleting files...")
+
+    # We remove the files from the app directory
+    remove_local_file(local_model1_path)
+    remove_local_file(local_model2_path)
+    remove_local_file(local_preprocess_path)
+
+    gc.collect()
+    return loaded_model_pnns1, loaded_model_pnns2, loaded_preprocess_input
---
+++ app-classification-pnns-groups/main.py
@@ -0,0 +1,117 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import JSONResponse
+from io import BytesIO
+import base64
+import tensorflow as tf
+from functions.loading_models import load_API_models
+import gc
+
+app = FastAPI()
+
+# We load the models and the preprocess
+loaded_model_pnns1, loaded_model_pnns2, loaded_preprocess_input = load_API_models()
+
+def load_and_preprocess_image(image_path):
+    # Load and preprocess a single image
+    with tf.keras.preprocessing.image.load_img(image_path, target_size=(299, 299)) as img:
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)  # Create batch dimension
+        
+
+        gc.collect()
+        # We use the loaded preprocess
+        return loaded_preprocess_input(img_array)
+
+def predict_on_image(model, image_path):
+    preprocessed_image = load_and_preprocess_image(image_path)
+    predictions = model.predict(preprocessed_image)
+    gc.collect()
+    
+    return predictions
+
+def img_prediction_model_pnns(model_pnns, pnns_groups, image):
+    
+    if pnns_groups == "pnns_groups_1":
+        list_pnns = ['Composite foods', 'Fruits and vegetables', 'Cereals and potatoes', 'Fat and sauces',
+                    'Salty snacks', 'Beverages', 'Sugary snacks', 'Fish Meat Eggs', 'Milk and dairy products']
+    
+    elif pnns_groups == "pnns_groups_2":
+        list_pnns = ['Pizza pies and quiches', 'Fruits', 'Bread', 'Dressings and sauces', 'Salty and fatty products',
+                     'Sweetened beverages', 'Sweets', 'Biscuits and cakes', 'Pastries', 'Fish and seafood',
+                     'One-dish meals', 'Appetizers', 'Cheese', 'Fats', 'Processed meat', 'Sandwiches',
+                     'Vegetables', 'Breakfast cereals', 'Chocolate products', 'Unsweetened beverages',
+                     'Dried fruits', 'Meat', 'Cereals', 'Eggs', 'Plant-based milk substitutes', 'Legumes',
+                     'Teas and herbal teas and coffees', 'Dairy desserts', 'Nuts', 'Fruit juices',
+                     'Milk and yogurt', 'Artificially sweetened beverages', 'Potatoes', 'Soups', 'Ice cream',
+                     'Offals', 'Waters and flavored waters', 'Fruit nectars']
+
+    result = predict_on_image(model_pnns, image)
+    
+    image_proba = []
+    
+    for pnns, proba in zip(list_pnns, result[0]):
+        image_proba.append([pnns, proba])
+
+    # Using a list of dictionaries
+    image_proba = list(zip(list_pnns, result[0].tolist()))
+    image_proba.sort(key=lambda x: x[1], reverse=True)
+
+    # Convert the sorted list of tuples to a list of dictionaries
+    image_proba = [{"pnns_groups": pnns, "probabilities": proba} for pnns, proba in image_proba]
+    
+    del result
+    gc.collect()
+
+    return image_proba
+
+# Alternative to numpy.argmax 
+def argmax(result):
+    if len(result) == 1: # When list is nested
+        return max(range(len(result[0])), key=result[0].__getitem__)
+    else: 
+        return max(range(len(result)), key=result.__getitem__)
+
+@app.get("/")
+def home():
+    return {"health_check": "OK"}
+
+@app.post("/process-image/")
+async def process_image_endpoint(
+    file: UploadFile = File(None), 
+    base64_image: str = Form(None), 
+    pnns_groups: str = Form(None)):
+
+    try:
+        if file:
+        # If file image is provided, decode it
+
+            # Uploaded file as to be one of those extension
+            if file.content_type not in ["image/png", "image/jpeg", "image/jpg"]:
+                raise ValueError("Only PNG, JPEG or JPG images are allowed.")
+
+            # Read the file content
+            file_contents = await file.read()
+
+
+        elif base64_image:
+        # If base64 image is provided, we decode it
+
+            data_split = base64_image.split('base64,')
+            
+            encoded_data = data_split[1]
+            
+            file_contents = base64.b64decode(encoded_data)
+
+        # We do the prediction
+        if pnns_groups == "pnns_groups_1":
+            result = img_prediction_model_pnns(loaded_model_pnns1, pnns_groups, BytesIO(file_contents), loaded_preprocess_input)
+        elif pnns_groups == "pnns_groups_2":
+            result = img_prediction_model_pnns(loaded_model_pnns2, pnns_groups, BytesIO(file_contents), loaded_preprocess_input)
+        
+        gc.collect()
+
+        #df_result = df_result.to_dict(orient='records')
+        return JSONResponse(content=result)
+
+    except Exception as e:
+        return JSONResponse(content={"status": "error", "message": str(e)})
